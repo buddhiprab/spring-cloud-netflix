a85488f1986b354828d595647d129efe99940df5
Buddhi Nagahawatte <SGARNABN@rhc.msig.com>
2019-10-02 17:48:42 +0800

    init

---
+++ account/src/main/java/com/buddhi/cloud/AccountService.java
@@ -0,0 +1,22 @@
+package com.buddhi.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class AccountService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountService.class, args);
+	}
+
+	@GetMapping("/")
+	public String getAccount(){
+		return "account 1";
+	}
+}
